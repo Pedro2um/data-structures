8dfd342caffe7185a7c0bc5f5270792c887fc4f0
Pedro2um1dois <pedroigorgm@gmail.com>
2022-03-31 17:26:56 -0300

    Update stack.h

--- stack/stack.h
+++ stack/stack.h
@@ -24,8 +24,6 @@ static void     reverse_string(char* string);
 //check for balanced parentheses, curly braces and brackets 
 static bool     is_balanced(char* string);
 
-static char     opposite_of_my_close_char(char c);
-
 // Evaluating expression problems of Infix operations
 // 1) Parentheses and so on (), {}, [] - is deal by balanced parentheses function  
 // 2) Exponents - right to left 
@@ -41,12 +39,14 @@ static char     opposite_of_my_close_char(char c);
 //      Reverse Polish notation
 //      Postfix:        a b c * +
 //
-
+static char     opposite_of_my_close_char(char c);
 static void     change_all_specifiers_to_parentheses(char* string);
 static bool     has_higher_prec(char x, char y);
 static void     infix_to_postfix_equation(char* string, char equation_flag);
 
 static bool     is_operator(char c);
+
+//somenthing that i learn for Competitive Programing
 static int      fast_power(int a, int b);
 static int      perform_operation(char c, int x, int y);
 static void     evaluate_postfix_equation(char* string, char equation_flag);
